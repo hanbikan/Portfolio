bdac6a663c134d055b85a26beba48709e848f93e
hanbit-kang <fchopinof99@gmail.com>
2021-03-06 15:24:51 +0900

    1. YSCT 추가 2. Carousel 세로로 길 때 CSS 추가

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -12,6 +12,10 @@ import img_classical5 from '../images/portfolio/classical/5.png';
 import img_portfolio from '../images/portfolio/portfolio/main.png';
 import img_portfolio1 from '../images/portfolio/portfolio/1.png';
 import img_portfolio2 from '../images/portfolio/portfolio/2.png';
+import img_YSCT from '../images/portfolio/YSCT/main.png';
+import img_YSCT1 from '../images/portfolio/YSCT/1.png';
+import img_YSCT2 from '../images/portfolio/YSCT/2.png';
+import img_YSCT3 from '../images/portfolio/YSCT/3.png';
 
 class Portfolio extends React.Component {
   state = {
@@ -46,20 +50,20 @@ class Portfolio extends React.Component {
         <div className="portfolio_list">
           <div className="portfolio object">
             <div className="pf_carousel" onClick={this.handleClick}>
-              <Carousel className="carousel" interval={1000000}>
-                <Carousel.Item>
+              <Carousel className="carousel-width" interval={1000000}>
+                <Carousel.Item className="carousel_item">
                   <img className="d-block img-fluid" src={img_classical1} alt="Classical" />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className="carousel_item">
                   <img className="d-block img-fluid" src={img_classical2} alt="Classical" />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className="carousel_item">
                   <img className="d-block img-fluid" src={img_classical3} alt="Classical" />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className="carousel_item">
                   <img className="d-block img-fluid" src={img_classical4} alt="Classical" />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className="carousel_item">
                   <img className="d-block img-fluid" src={img_classical5} alt="Classical" />
                 </Carousel.Item>
               </Carousel>
@@ -96,12 +100,12 @@ class Portfolio extends React.Component {
           </div>
           <div className="portfolio object">
             <div className="pf_carousel" onClick={this.handleClick}>
-              <Carousel className="carousel" interval={1000000}>
-                <Carousel.Item>
-                  <img className="d-block img-fluid" src={img_portfolio1} alt="Classical" />
+              <Carousel className="carousel-width" interval={1000000}>
+                <Carousel.Item className="carousel_item">
+                  <img className="d-block img-fluid" src={img_portfolio1} alt="Portfolio" />
                 </Carousel.Item>
-                <Carousel.Item>
-                  <img className="d-block img-fluid" src={img_portfolio2} alt="Classical" />
+                <Carousel.Item className="carousel_item">
+                  <img className="d-block img-fluid" src={img_portfolio2} alt="Portfolio" />
                 </Carousel.Item>
               </Carousel>
               <a className="carousel_cancel color-white">닫기</a>
@@ -128,6 +132,49 @@ class Portfolio extends React.Component {
                 <button className="pf_link" onClick={(e) => {
                   e.preventDefault();
                   window.open('https://github.com/hanbit-kang/Portfolio', '_blank');
+                }}>
+                  Link {'>'}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="portfolio object">
+            <div className="pf_carousel" onClick={this.handleClick}>
+              <Carousel className="carousel-height" interval={1000000}>
+                <Carousel.Item className="carousel_item">
+                  <img className="d-block img-fluid-height" src={img_YSCT1} alt="이건 아셔야겠는데요" />
+                </Carousel.Item>
+                <Carousel.Item className="carousel_item">
+                  <img className="d-block img-fluid-height" src={img_YSCT2} alt="이건 아셔야겠는데요" />
+                </Carousel.Item>
+                <Carousel.Item className="carousel_item">
+                  <img className="d-block img-fluid-height" src={img_YSCT3} alt="이건 아셔야겠는데요" />
+                </Carousel.Item>
+              </Carousel>
+              <a className="carousel_cancel color-white">닫기</a>
+            </div>
+            <div className="pf_top" onClick={this.handleClick}>
+              <img src={img_YSCT} className="pf_img" />
+            </div>
+            <div className="pf_bottom">
+              <div className="pf_bottom_header">
+                <p className="pf_title">이건 아셔야겠는데요</p>
+              </div>
+              <div className="pf_bottom_content">
+                <p className="pf_text">
+                  이 어플은 주식 어플입니다. 관심종목을 등록하고 조건을 설정하고,
+                  만약 주식이 이 조건 기준을 초과하면 Notification으로 알려주는 기능을 수행합니다.
+                  2021년 2월에 안드로이드 네이티브 공부를 시작하면서 만든 앱입니다.
+                </p>
+                <div className="pf_tags">
+                  <a className="tag cursor-pointer">#Android_Native </a>
+                  <a className="tag cursor-pointer">#Kotlin </a>
+                </div>
+              </div>
+              <div className="pf_bottom_footer">
+                <button className="pf_link" onClick={(e) => {
+                  e.preventDefault();
+                  window.open('https://github.com/Hanbit-Kang/YouShouldCheckThis', '_blank');
                 }}>
                   Link {'>'}
                 </button>
