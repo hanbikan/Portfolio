85ec414f5b10e5d3e9459d1df2b4684b50aab72a
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-04-16 22:07:51 +0900

    1. About UI 수정 2. About 깃허브, 이메일 추가

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './about.css';
 import './md.css';
-import img_portrait from '../images/portrait.jpg';
+import img_portrait from '../images/about/portrait.jpg';
+import img_github from '../images/github.png';
+import img_gmail from '../images/about/gmail.png';
 
 class About extends React.Component {
   render(){
@@ -28,20 +31,37 @@ class About extends React.Component {
               <p className="ai_portrait_text">
                 세종대학교 컴퓨터공학과 18학번
               </p>
+
+              <div className="ai_link_area">
+                <span className="ai_link">
+                  <img src={img_gmail} className="ai_link_icon"/>
+                  <CopyToClipboard text={"fchopinof99@gmail.com"} onCopy={()=>alert("이메일 주소를 복사하였습니다.")}>
+                    <a className="ai_link_src color-black cursor-pointer">
+                      fchopinof99@gmail.com
+                    </a>
+                  </CopyToClipboard>
+                </span>
+                <span className="ai_link">
+                  <img src={img_github} className="ai_link_icon"/>
+                  <a className="ai_link_src color-black cursor-pointer" href="https://github.com/hanbit-kang" target="_blank">
+                    https://github.com/hanbit-kang
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
 
           <div className="ai_text_area object">
             <p className="double_quotes">“</p>
-            <p className="ai_text">
-              저는 <b>코딩을 즐기는</b> 개발자입니다.
-            </p>
-            <p className="ai_text">
-              초등학생 3학년부터 6학년 때까지, 총 35개의 게임을 직접 만들어 놀면서 개발자라는 꿈을 키웠습니다.
-            </p>
-            <p className="ai_text">
-              대학생이 된 저는 개인 프로젝트를 항상 진행하고 있는데, 이것은 제가 코딩을 진심으로 재밌어 하기 때문입니다.
-            </p>
+              <p className="ai_text">
+                저는 <i>코딩을 즐기는 개발자</i> 입니다.
+              </p>
+              <p className="ai_text">
+                초등생 3학년에 시작하여 3년간, 총 35개의 게임을 직접 만들어보면서 개발자라는 꿈을 키웠습니다.
+              </p>
+              <p className="ai_text">
+                대학생이 된 저는 개인 프로젝트를 항상 진행하는데, 이것은 제가 코딩을 진심으로 좋아하기 때문입니다.
+              </p>
             <p className="double_quotes">”</p>
           </div>
         </div>
