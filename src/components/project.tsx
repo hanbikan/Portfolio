5eb1a06415f5078063597d3779adbdcb30cb1ac9
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-06-29 18:01:37 +0900

    Add Bitcoin-Mining-Game in Project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -4,6 +4,7 @@ import './project.css';
 import './md.css';
 import Carousel from 'react-bootstrap/Carousel';
 import img_github from '../images/github.png';
+import img_googleplay from '../images/googleplay.png';
 import img_classical from '../images/project/classical/main.png';
 import img_classical1 from '../images/project/classical/1.png';
 import img_classical2 from '../images/project/classical/2.png';
@@ -15,6 +16,10 @@ import img_YSCT from '../images/project/YSCT/main.png';
 import img_YSCT1 from '../images/project/YSCT/1.png';
 import img_YSCT2 from '../images/project/YSCT/2.png';
 import img_YSCT3 from '../images/project/YSCT/3.png';
+import img_BMG from '../images/project/BMG/main.png';
+import img_BMG1 from '../images/project/BMG/1.png';
+import img_BMG2 from '../images/project/BMG/2.png';
+import img_BMG3 from '../images/project/BMG/3.png';
 
 class Project extends React.Component {
   state = {
@@ -96,7 +101,7 @@ class Project extends React.Component {
                     e.preventDefault();
                     window.open('https://github.com/hanbit-kang/Classical', '_blank');
                   }}>
-                    <img src={img_github} className="github-img"/>
+                    <img src={img_github} className="link-img"/>
                     Repository {'>'}
                   </button>
                 </div>
@@ -129,13 +134,14 @@ class Project extends React.Component {
                     e.preventDefault();
                     window.open('https://github.com/hanbit-kang/Portfolio', '_blank');
                   }}>
-                    <img src={img_github} className="github-img"/>
+                    <img src={img_github} className="link-img"/>
                     Repository {'>'}
                   </button>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="project object">
             <div className="project__carousel" onClick={this.handleClick}>
               <Carousel className="carousel-height" interval={1000000}>
@@ -176,13 +182,60 @@ class Project extends React.Component {
                     e.preventDefault();
                     window.open('https://github.com/hanbit-kang/AndroidNotificationForStock', '_blank');
                   }}>
-                    <img src={img_github} className="github-img"/>
+                    <img src={img_github} className="link-img"/>
                     Repository {'>'}
                   </button>
                 </div>
               </div>
             </div>
 
+          <div className="project object">
+            <div className="project__carousel" onClick={this.handleClick}>
+              <Carousel className="carousel-height" interval={1000000}>
+                <Carousel.Item className="project__carousel__item">
+                  <img className="d-block img-fluid-height" src={img_BMG1} alt="비트코인 채굴장" />
+                </Carousel.Item>
+                <Carousel.Item className="project__carousel__item">
+                  <img className="d-block img-fluid-height" src={img_BMG2} alt="비트코인 채굴장" />
+                </Carousel.Item>
+                <Carousel.Item className="project__carousel__item">
+                  <img className="d-block img-fluid-height" src={img_BMG3} alt="비트코인 채굴장" />
+                </Carousel.Item>
+              </Carousel>
+              <a className="project__carousel__cancel color-white">닫기</a>
+            </div>
+
+            <div className="project-info">
+              <div className="project-img-area" onClick={this.handleClick}>
+                <img src={img_BMG} className="project__img" />
+              </div>
+              <div className="project-description">
+                <div className="project-description__header">
+                  <p className="project__title">비트코인 채굴장</p>
+                </div>
+                <div className="project-description__content">
+                  <p className="project-description__content__text">
+                    이 어플은 비트코인 채굴장 컨셉의 모바일 게임입니다.
+                    오버클럭, 컴퓨터, GPU를 구입해가면서 채굴장을 키워나갈 수 있습니다.
+                    비트코인 매매 시스템에서는, 실제 비트코인 시세에 따라 코인을 매매할 수 있습니다.
+                  </p>
+                  <div className="tag-area">
+                    <a className="tag cursor-pointer">#C# </a>
+                    <a className="tag cursor-pointer">#Unity </a>
+                  </div>
+                </div>
+                <div className="project-description__footer">
+                <button className="project__button" onClick={(e) => {
+                    e.preventDefault();
+                    window.open('https://play.google.com/store/apps/details?id=com.KangHam.BitcoinMiningGame', '_blank');
+                  }}>
+                    <img src={img_googleplay} className="link-img"/>
+                    Google Play {'>'}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </div>
